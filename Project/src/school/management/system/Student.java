f483b911538d8dd10e93708fed674b2ada19452c
Nafiz Fahad <Fahad Nafiz>
2020-08-30 20:49:42 +0600

    Hlw

--- Project/src/school/management/system/Student.java
+++ Project/src/school/management/system/Student.java
@@ -3,11 +3,11 @@ package school.management.system;
 public class Student {
 	private int id;
 	private String name;
-	private int grade;
+	private double grade;
 	private int feesPaid;
 	private int feesTotal;
 	
-	public Student(int id,String name,int grade) {
+	public Student(int id,String name,double grade) {
 		this.feesPaid=0;
 		this.feesTotal=30000;
 		this.id=id;
@@ -16,12 +16,14 @@ public class Student {
 		
 		
 	}
- public void setGrade(int grade)
- {
-	this.grade=grade;
- }
- public void updateFessPaid(int fees) {
+   public void setGrade(double grade)
+   {
+	   this.grade=grade;
+   }
+   public void updateFessPaid(int fees) {
+	   feespaid+=fees;
+	   
 	 
- }
+    }
 
 }
